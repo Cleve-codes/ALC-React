08387c15624409b822ba380e02e254dc1977f926
Cleve <clevemomanyi@gmail.com>
2023-08-03 12:36:29 +0300

    numbers'

--- 0x03-spa-jsx/src/App.js
+++ 0x03-spa-jsx/src/App.js
@@ -1,12 +1,20 @@
 import "./index.css";
-
-const ints = [1, 2, 3];
+import React from "react";
+//import { ReactDOM } from "react-dom/client";
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState([1, 2, 3]);
+
+  const addNum = () => {
+    const newNum = Math.max(...num) + 1;
+    setNum([...num, newNum]);
+  };
+
   return (
     <div className="App">
       <ul>
-        <ListItems />
+        <ListItems num={num} addNum={addNum} />
       </ul>
     </div>
   );
@@ -14,11 +22,12 @@ function App() {
 
 export default App;
 
-function ListItems() {
+function ListItems({ num, addNum }) {
   return (
     <>
-      {ints.map((int) => (
-        <li>{int}</li>
+      <button onClick={addNum}>Add Item</button>
+      {num.map((int) => (
+        <li key={int}>{int}</li>
       ))}
     </>
   );
