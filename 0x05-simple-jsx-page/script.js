581e7d186743871f0a42a32aad771c350d43e4e7
Cleve <clevemomanyi@gmail.com>
2023-08-03 21:04:46 +0300

    spa

---
+++ 0x05-simple-jsx-page/script.js
@@ -0,0 +1,14 @@
+const jsDiv = document.getElementById("dateJs");
+const reactDiv = document.getElementById("dateJsx");
+
+const render = () => {
+  jsDiv.innerHTML = `
+    <div class="split">
+    Js template
+    <input />
+    <p>${new Date()}</p>
+    </div>
+    `;
+};
+
+setInterval(render, 1000);
