7efd52f89b1964f494471479e1430d87de561545
Cleve <clevemomanyi@gmail.com>
2023-08-04 19:59:18 +0300

    hooks

--- 0x04-globomantics/src/main-page/App.js
+++ 0x04-globomantics/src/main-page/App.js
@@ -1,6 +1,26 @@
+import { useEffect, useState, useMemo } from "react";
 import Header from "./Header";
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const response = await fetch("./houses.json");
+      const houses = response.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(()=>{
+     if (allHouses.length) {
+       const randomHouse = Math.floor(Math.random() * allHouses.length);
+       return allHouses[randomHouse];
+     }
+  }, [allHouses])
+ 
+
   return (
     <div className="container">
       <Header />
