9501de5b1e30625a143dabb6c9251d951c6e94ff
Cleve <clevemomanyi@gmail.com>
2023-08-07 23:38:30 +0300

    changes

--- 0x07-use-ref/src/App.js
+++ 0x07-use-ref/src/App.js
@@ -1,36 +1,9 @@
-import { useState } from "react";
-import bob1 from "./bob-marley-1.png";
-import bob2 from "./bob-marley-2.png";
-import bob3 from "./bob-marley-3.png";
-import bob4 from "./bob-marley-4.png";
+import ImageHovered from "./ImageHovered";
 
 function App() {
-  const [hovered, setHovered] = useState(null);
-
-  const onHover = (e) => {
-    setHovered(e.target.id);
-  };
-
-  const onHoverOut = (e) => {
-    setHovered(null);
-  };
-
   return (
     <div className="App">
-      <img
-        src={hovered === "1" ? bob1 : bob3}
-        alt="Bob marley"
-        onMouseEnter={onHover}
-        onMouseLeave={onHoverOut}
-        id="1"
-      />
-      <img
-        src={hovered === "2" ? bob2 : bob4}
-        alt="Bob marley"
-        onMouseEnter={onHover}
-        onMouseLeave={onHoverOut}
-        id="2"
-      />
+      <ImageHovered />
     </div>
   );
 }
---
+++ 0x07-use-ref/src/ImageHovered.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from "react";
+import bob1 from "./bob-marley-1.png";
+import bob2 from "./bob-marley-2.png";
+import bob3 from "./bob-marley-3.png";
+import bob4 from "./bob-marley-4.png";
+
+const ImageHovered = () => {
+  const [hovered, setHovered] = useState(null);
+
+     const onHover = (e) => {
+       setHovered(e.target.id);
+     };
+
+     const onHoverOut = (e) => {
+       setHovered(null);
+     };
+
+
+  return (
+    <>
+      <img
+        src={hovered === "1" ? bob1 : bob3}
+        alt="Bob marley"
+        onMouseEnter={onHover}
+        onMouseLeave={onHoverOut}
+        id="1"
+      />
+
+      <img
+        src={hovered === "2" ? bob2 : bob4}
+        alt="Bob marley"
+        onMouseEnter={onHover}
+        onMouseLeave={onHoverOut}
+        id="2"
+      />
+    </>
+  );
+}
+
+export default ImageHovered
