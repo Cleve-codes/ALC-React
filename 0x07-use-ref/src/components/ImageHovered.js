3aa9efd95b3cb206ce169de8206aac1c1d2046d6
Cleve <clevemomanyi@gmail.com>
2023-08-08 07:59:31 +0300

    bugged

--- 0x07-use-ref/src/components/ImageHovered.js
+++ 0x07-use-ref/src/components/ImageHovered.js
@@ -1,41 +1,42 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
-import bob1 from "./Images/bob-marley-1.png";
-import bob2 from "./Images/bob-marley-2.png";
-import bob3 from "./Images/bob-marley-3.png";
-import bob4 from "./Images/bob-marley-4.png";
+
+  
+const images = [
+  "./Images/bob-marley-1.png",
+  "./Images/bob-marley-2.png",
+  "./Images/bob-marley-3.png",
+  "./Images/bob-marley-4.png",
+];
 
 const ImageHovered = () => {
   const [hovered, setHovered] = useState(null);
 
-     const onHover = (e) => {
-       setHovered(e.target.id);
-     };
+  const onHover = (e) => {
+    setHovered(e.target.id);
+  };
 
-     const onHoverOut = (e) => {
-       setHovered(null);
-     };
-
+  const onHoverOut = (e) => {
+    setHovered(null);
+  };
 
   return (
     <>
-      <img
-        src={hovered === "1" ? bob1 : bob3}
-        alt="Bob marley"
-        onMouseEnter={onHover}
-        onMouseLeave={onHoverOut}
-        id="1"
-      />
-
-      <img
-        src={hovered === "2" ? bob2 : bob4}
-        alt="Bob marley"
-        onMouseEnter={onHover}
-        onMouseLeave={onHoverOut}
-        id="2"
-      />
+      {Array.from({ length: 4 }).map((_, i) => {
+        return (
+          <img
+            src={images[i]}
+            alt={`bob marley ${i + 1}`}
+            onMouseEnter={onHover}
+            onMouseLeave={onHoverOut}
+            key={i}
+            id={i}
+          />
+        );
+      })}
+      
     </>
   );
-}
+};
 
-export default ImageHovered
+export default ImageHovered;
