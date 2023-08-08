0343d187620bdfc078d9296ea0ce07106e7ed2c2
Cleve <clevemomanyi@gmail.com>
2023-08-08 08:35:45 +0300

    debuggged

--- 0x07-use-ref/src/components/ImageHovered.js
+++ 0x07-use-ref/src/components/ImageHovered.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useState } from "react";
+import bob1 from "./Images/bob-marley-1.png";
+import pac1 from "./Images/Tupac-1.png";
+import bob2 from "./Images/bob-marley-2.png";
+import pac2 from "./Images/Tupac-2.png";
+import bob3 from "./Images/bob-marley-3.png";
+import pac3 from "./Images/Tupac-3.png";
+import bob4 from "./Images/bob-marley-4.png";
+import pac4 from "./Images/Tupac-4.png";
 
-  
-const images = [
-  "./Images/bob-marley-1.png",
-  "./Images/bob-marley-2.png",
-  "./Images/bob-marley-3.png",
-  "./Images/bob-marley-4.png",
-];
+const original = [bob1, bob2, pac1, pac2];
+const blackNwhite = [bob3, bob4, pac4, pac3];
 
 const ImageHovered = () => {
   const [hovered, setHovered] = useState(null);
@@ -25,16 +28,15 @@ const ImageHovered = () => {
       {Array.from({ length: 4 }).map((_, i) => {
         return (
           <img
-            src={images[i]}
-            alt={`bob marley ${i + 1}`}
+            src={hovered === `${i + 1}` ? original[i] : blackNwhite[i]}
+            alt={i + 1}
+            id={i + 1}
             onMouseEnter={onHover}
             onMouseLeave={onHoverOut}
             key={i}
-            id={i}
           />
         );
       })}
-      
     </>
   );
 };
